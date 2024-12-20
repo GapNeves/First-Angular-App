bec72ba6deac4f456e17cb231d114eb5d81efc36
GapNeves <gabriel.neves84@hotmail.com>
2024-12-20 20:10:06 -0300

    :sparkles: Feat: Automatically creating tasks field

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,5 +9,33 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
-  @Input() name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  tasks = [
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Master Angular',
+      summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+      dueDate: '2025-12-31'
+    },
+    {
+      id: 't2',
+      userId: 'u2',
+      title: 'Build first prototype',
+      summary: 'Build first prototype of the online shop website.',
+      dueDate: '2025-05-31'
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title: 'Prepare issue template',
+      summary: 'Prepare ande describe an issue template which will help with project management.',
+      dueDate: '2024-06-15'
+    },
+  ];
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
